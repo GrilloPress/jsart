faaa00ad3568dcb0ae2c01af6fe8f4743762d3f7
GrilloPress <grillopress@gmail.com>
2015-10-31 23:07:33 +0000

    add colour randomizer to first example

--- sketch.js
+++ sketch.js
@@ -4,9 +4,16 @@ function setup() {
 
 function draw() {
   if (mouseIsPressed) {
-    fill(0);
+    // using RGB values we make each choose a random number and inject that into the fill method when a button is clicked
+    
+    var r = random(255);
+    var g = random(255);
+    var b = random(255);
+    fill(r, g, b);
   } else {
-    fill(255);
+    
+    var greyscale = random(255);
+    fill(greyscale);
   }
   ellipse(mouseX, mouseY, 80, 80);
 }
